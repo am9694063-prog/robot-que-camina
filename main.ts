39eac949e08806fadc4d586dccbaf8b3657d18fc
am9694063-prog <am9694063@gmail.com>
2025-11-25 22:54:23 -0300

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
-if (pins.digitalReadPin(DigitalPin.P1) == 0) {
+if (pins.digitalReadPin(DigitalPin.P1) == 2) {
     basic.showIcon(IconNames.Yes)
+} else if (false) {
+	
 } else {
     basic.showIcon(IconNames.StickFigure)
 }
